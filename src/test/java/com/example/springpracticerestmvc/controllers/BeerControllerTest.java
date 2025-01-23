4e88dc619eb17c545725c053db517ead5f530700
noobdevsam <mdshoykotislam05@gmail.com>
2025-01-23 18:53:06 +0600

    using json path matcher with mockito

--- src/test/java/com/example/springpracticerestmvc/controllers/BeerControllerTest.java
+++ src/test/java/com/example/springpracticerestmvc/controllers/BeerControllerTest.java
@@ -7,16 +7,14 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.example.springpracticerestmvc.services.BeerService;
 import com.example.springpracticerestmvc.services.impl.BeerServiceImpl;
 
-import java.util.UUID;
 
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
@@ -34,11 +32,16 @@ class BeerControllerTest {
 
         var beer = beerServiceImpl.listBeers().get(0);
 
-        given(beerService.getBeerById(any(UUID.class))).willReturn(beer);
+        //given(beerService.getBeerById(any(UUID.class))).willReturn(beer);
+        given(beerService.getBeerById(beer.getId())).willReturn(beer);
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString())
+        // using json path matcher to access json object
+        // such as $.id or $.beerName
+        mockMvc.perform(get("/api/v1/beer/" + beer.getId())
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(beer.getId().toString())))
+                .andExpect(jsonPath("$.beerName", is(beer.getBeerName())));
     }
 }

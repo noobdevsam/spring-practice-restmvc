8e5a185f4040352af9f8b0844541dc49c5b46e53
noobdevsam <mdshoykotislam05@gmail.com>
2025-01-23 19:27:03 +0600

    adding test for list beers

--- src/test/java/com/example/springpracticerestmvc/controllers/BeerControllerTest.java
+++ src/test/java/com/example/springpracticerestmvc/controllers/BeerControllerTest.java
@@ -38,10 +38,22 @@ class BeerControllerTest {
         // using json path matcher to access json object
         // such as $.id or $.beerName
         mockMvc.perform(get("/api/v1/beer/" + beer.getId())
-                .accept(MediaType.APPLICATION_JSON))
+                    .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is(beer.getId().toString())))
                 .andExpect(jsonPath("$.beerName", is(beer.getBeerName())));
     }
+
+    @Test
+    void test_list_beers() throws Exception {
+        given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
+
+        mockMvc.perform(get("/api/v1/beer")
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.length()", is(3)));
+    }
+
 }

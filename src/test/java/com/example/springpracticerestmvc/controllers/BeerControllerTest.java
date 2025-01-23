8863d35c76a8de04398630fa8ff898ee88df5097
noobdevsam <mdshoykotislam05@gmail.com>
2025-01-24 02:08:50 +0600

    creating new beer and using it with ObjectMapper

--- src/test/java/com/example/springpracticerestmvc/controllers/BeerControllerTest.java
+++ src/test/java/com/example/springpracticerestmvc/controllers/BeerControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.springpracticerestmvc.controllers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,12 +9,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
 
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import com.example.springpracticerestmvc.model.Beer;
 import com.example.springpracticerestmvc.services.BeerService;
 import com.example.springpracticerestmvc.services.impl.BeerServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @WebMvcTest(BeerController.class)
@@ -25,7 +30,16 @@ class BeerControllerTest {
     @MockitoBean
     BeerService beerService;
 
-    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+    BeerServiceImpl beerServiceImpl;
+
+    @Autowired
+    ObjectMapper objectMapper;
+    // for serializing-deserializing json object to pojo and vice versa
+
+    @BeforeEach
+    void setUp() {
+        beerServiceImpl = new BeerServiceImpl();
+    }
 
     @Test
     void getBeerById() throws Exception {
@@ -54,6 +68,29 @@ class BeerControllerTest {
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(jsonPath("$.length()", is(3)));
+    }
+
+    @Test
+    void test_create_new_beer() throws Exception {
+        var beer = beerServiceImpl.listBeers().get(0);
+        beer.setId(null);
+        beer.setVersion(null);
+
+        given(beerService.saveNewBeer(any(Beer.class)))
+            .willReturn(beerServiceImpl.listBeers().get(1));
+
+        mockMvc.perform(
+            post("/api/v1/beer")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(beer))
+        ).andExpect(status().isCreated())
+        .andExpect(header().exists("Location"));
+
+        // writing to json
+        //System.out.println(objectMapper.writeValueAsString(beer));
+
+
     }
 
 }

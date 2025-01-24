03ba1a05f88e1ed2e6f3f09e17888a324031825d
noobdevsam <mdshoykotislam05@gmail.com>
2025-01-24 18:25:34 +0600

    testing customer update

--- src/test/java/com/example/springpracticerestmvc/controllers/CustomerControllerTest.java
+++ src/test/java/com/example/springpracticerestmvc/controllers/CustomerControllerTest.java
@@ -12,11 +12,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CustomerController.class)
@@ -80,4 +82,36 @@ public class CustomerControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(header().exists("Location"));
     }
+
+    @Test
+    void test_update_customer() throws Exception {
+        var customer = customerServiceImpl.getAllCustomers().getFirst();
+
+        mockMvc.perform(
+                put("/api/v1/customer/" + customer.getId())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customer))
+        ).andExpect(status().isNoContent());
+
+        // Verifies certain behavior (happened once).
+        verify(customerService).updateCustomerById(any(UUID.class), any(Customer.class));
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

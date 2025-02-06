fcb7b6f69c3f8f607352e722ee2e804a04ede192
noobdevsam <mdshoykotislam05@gmail.com>
2025-02-07 02:38:54 +0600

    adding tests for customer entity with jpa

--- src/main/java/com/example/springpracticerestmvc/services/impl/CustomerServiceJpaImpl.java
+++ src/main/java/com/example/springpracticerestmvc/services/impl/CustomerServiceJpaImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Profile("jpa")
@@ -24,12 +25,17 @@ public class CustomerServiceJpaImpl implements CustomerService {
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
-        return List.of();
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Optional<CustomerDTO> getCustomerById(UUID id) {
-        return Optional.empty();
+        return Optional.ofNullable(customerMapper.customerToCustomerDto(
+                customerRepository.findById(id).orElse(null)
+        ));
     }
 
     @Override

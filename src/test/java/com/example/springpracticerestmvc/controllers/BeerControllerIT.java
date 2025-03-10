57c59966a2311c93f2112ffdc2d87a199803ceb1
noobdevsam <mdshoykotislam05@gmail.com>
2025-03-10 14:24:29 +0600

    adding jpa controller test

--- src/test/java/com/example/springpracticerestmvc/controllers/BeerControllerIT.java
+++ src/test/java/com/example/springpracticerestmvc/controllers/BeerControllerIT.java
@@ -4,17 +4,27 @@ import com.example.springpracticerestmvc.exceptions.NotFoundException;
 import com.example.springpracticerestmvc.mappers.BeerMapper;
 import com.example.springpracticerestmvc.model.BeerDTO;
 import com.example.springpracticerestmvc.repositories.BeerRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class BeerControllerIT {
@@ -27,6 +37,19 @@ class BeerControllerIT {
 
     @Autowired
     BeerMapper beerMapper;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
     @Test
     void test_list_beers() {
@@ -111,6 +134,23 @@ class BeerControllerIT {
     @Test
     void test_delete_by_id_not_found() {
         assertThrows(NotFoundException.class, () -> beerController.deleteById(UUID.randomUUID()));
+    }
+
+    @Test
+    void test_patch_beer_bad_name() throws Exception{
+        var beer = beerRepository.findAll().getFirst();
+
+        Map<String, Object> beer_map = new HashMap<>();
+        beer_map.put("beerName", "New Name slkdjfslkjfoieujwoiekdslmcxmvnerkjsdfjslkj");
+
+        mockMvc.perform(
+                    patch(BeerController.BEER_PATH_ID, beer.getId())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(beer_map))
+
+                )
+                .andExpect(status().isBadRequest());
     }
 }
 

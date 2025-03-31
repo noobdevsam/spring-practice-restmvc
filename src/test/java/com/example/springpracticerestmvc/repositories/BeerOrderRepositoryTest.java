098a7c431335ac1e4d9dec3e5852c56fe7064d4c
noobdevsam <mdshoykotislam05@gmail.com>
2025-03-31 14:19:11 +0600

    performing persistence test

--- src/test/java/com/example/springpracticerestmvc/repositories/BeerOrderRepositoryTest.java
+++ src/test/java/com/example/springpracticerestmvc/repositories/BeerOrderRepositoryTest.java
@@ -1,11 +1,13 @@
 package com.example.springpracticerestmvc.repositories;
 
 import com.example.springpracticerestmvc.entities.Beer;
+import com.example.springpracticerestmvc.entities.BeerOrder;
 import com.example.springpracticerestmvc.entities.Customer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class BeerOrderRepositoryTest {
@@ -28,12 +30,24 @@ class BeerOrderRepositoryTest {
         beer = beerRepository.findAll().getFirst();
     }
 
+    @Transactional
     @Test
     void test_beer_order() {
-        System.out.println(beerOrderRepository.count());
-        System.out.println(customerRepository.count());
-        System.out.println(beerRepository.count());
-        System.out.println(customer.getName());
-        System.out.println(beer.getBeerName());
+//        System.out.println(beerOrderRepository.count());
+//        System.out.println(customerRepository.count());
+//        System.out.println(beerRepository.count());
+//        System.out.println(customer.getName());
+//        System.out.println(beer.getBeerName());
+
+        var saved_beer_order = beerOrderRepository.saveAndFlush(
+                BeerOrder.builder()
+                        .customerRef("Test order")
+                        .customer(customer)
+                        .build()
+        );
+
+        // This will cause a lazy loading exception, we need to use @Transactional
+        System.out.println(saved_beer_order.getCustomerRef());
+
     }
 }

5263fd8a11c995ba948b67b9449dec1cd1ef60d7
noobdevsam <mdshoykotislam05@gmail.com>
2025-04-12 00:07:36 +0600

    add default profile to BeerCsvServiceImpl and BootstrapData for improved environment support

--- src/main/java/com/example/springpracticerestmvc/bootstrap/BootstrapData.java
+++ src/main/java/com/example/springpracticerestmvc/bootstrap/BootstrapData.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ResourceUtils;
@@ -24,6 +25,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Profile({"localdb", "default"})
 public class BootstrapData implements CommandLineRunner {
 
     private final BeerRepository beerRepository;

c63a99fa47375dd9de977a8691dad6ee4f30b740
noobdevsam <mdshoykotislam05@gmail.com>
2025-06-17 00:31:14 +0600

    feat: implement BeerCreatedListener to persist audit records for beer creation events

--- src/main/java/com/example/springpracticerestmvc/listeners/BeerCreatedListener.java
+++ src/main/java/com/example/springpracticerestmvc/listeners/BeerCreatedListener.java
@@ -1,23 +1,35 @@
 package com.example.springpracticerestmvc.listeners;
 
 import com.example.springpracticerestmvc.events.BeerCreatedEvent;
+import com.example.springpracticerestmvc.mappers.BeerMapper;
+import com.example.springpracticerestmvc.repositories.BeerAuditRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class BeerCreatedListener {
+
+    private final BeerMapper beerMapper;
+    private final BeerAuditRepository beerAuditRepository;
 
     @Async
     @EventListener
     public void listen(BeerCreatedEvent event) {
-        System.out.println("I heard a beer was created!");
-        System.out.println("Beer Id: " + event.getBeer().getId());
 
-        System.out.println("Inside BeerCreatedListener");
-        System.out.println("Current thread name = " + Thread.currentThread().getName());
-        System.out.println("Current thread id = " + Thread.currentThread().threadId());
+        val beerAudit = beerMapper.beerToBeerAudit(event.getBeer());
+        beerAudit.setAuditEventType("BEER_CREATED");
 
-        // TODO: Implement - add real implementation to persist audit record
+        if (event.getAuthentication() != null && event.getAuthentication().getName() != null) {
+            beerAudit.setPrincipalName(event.getAuthentication().getName());
+        }
+
+        val savedBeerAudit = beerAuditRepository.save(beerAudit);
+        log.info("BeerAudit saved for BeerCreatedEvent Id : {}", savedBeerAudit.getId());
     }
 }

d358c424f52033d02c6619435c4dedb572cf7057
noobdevsam <mdshoykotislam05@gmail.com>
2025-06-17 18:51:26 +0600

    feat: add validation for tracking number in BeerOrderShipment and BeerOrderShipmentDTO

--- src/main/java/com/example/springpracticerestmvc/entities/BeerOrderShipment.java
+++ src/main/java/com/example/springpracticerestmvc/entities/BeerOrderShipment.java
@@ -1,6 +1,7 @@
 package com.example.springpracticerestmvc.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
@@ -30,6 +31,7 @@ public class BeerOrderShipment {
     @OneToOne
     private BeerOrder beerOrder;
 
+    @NotBlank
     private String trackingNumber;
 
     @CreationTimestamp

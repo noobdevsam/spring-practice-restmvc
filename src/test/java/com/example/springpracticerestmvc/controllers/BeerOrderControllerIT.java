fec73a6196ad0e0f799a4dbfa695ebc47231eb73
noobdevsam <mdshoykotislam05@gmail.com>
2025-06-19 12:27:07 +0600

    refactor: rename BeerOrderControllerTestIt to BeerOrderControllerIT for consistency

--- src/test/java/com/example/springpracticerestmvc/controllers/BeerOrderControllerIT.java
+++ src/test/java/com/example/springpracticerestmvc/controllers/BeerOrderControllerIT.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class BeerOrderControllerTestIt {
+class BeerOrderControllerIT {
 
     @Autowired
     WebApplicationContext wac;

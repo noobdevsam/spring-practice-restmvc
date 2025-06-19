1e19b0cdd77137fc5d3be5417da38a18c0ff402e
noobdevsam <mdshoykotislam05@gmail.com>
2025-06-19 11:56:30 +0600

    feat: add BeerOrderController with API path constants for beer orders

---
+++ src/main/java/com/example/springpracticerestmvc/controllers/BeerOrderController.java
@@ -0,0 +1,7 @@
+package com.example.springpracticerestmvc.controllers;
+
+public class BeerOrderController {
+
+    public static final String BEER_ORDER_PATH = "/api/v1/beer-order";
+    public static final String BEER_ORDER_PATH_ID = BEER_ORDER_PATH + "/{beerOrderId}";
+}

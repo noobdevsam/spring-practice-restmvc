a4e28781a9ceb72fe717747d209373c9c8e0935a
noobdevsam <mdshoykotislam05@gmail.com>
2025-06-19 14:04:42 +0600

    feat: add createOrder endpoint in BeerOrderController for order creation

--- src/main/java/com/example/springpracticerestmvc/controllers/BeerOrderController.java
+++ src/main/java/com/example/springpracticerestmvc/controllers/BeerOrderController.java
@@ -1,15 +1,15 @@
 package com.example.springpracticerestmvc.controllers;
 
 import com.example.springpracticerestmvc.exceptions.NotFoundException;
+import com.example.springpracticerestmvc.model.BeerOrderCreateDTO;
 import com.example.springpracticerestmvc.model.BeerOrderDTO;
 import com.example.springpracticerestmvc.services.BeerOrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.UUID;
 
 @RestController
@@ -29,12 +29,24 @@ public class BeerOrderController {
     }
 
     @GetMapping(BEER_ORDER_PATH)
-    public Page<BeerOrderDTO> listbeerOrders(
+    public Page<BeerOrderDTO> listBeerOrders(
             @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
             @RequestParam(value = "pageSize", required = false) Integer pageSize
     ) {
         return beerOrderService
                 .listOrders(pageNumber, pageSize);
+    }
+
+    @PostMapping(BEER_ORDER_PATH)
+    public ResponseEntity<Void> createOrder(
+            @RequestBody BeerOrderCreateDTO beerOrderCreateDTO
+    ) {
+
+        var savedOrder = beerOrderService.createOrder(beerOrderCreateDTO);
+
+        return ResponseEntity.created(
+                URI.create(BEER_ORDER_PATH + "/" + savedOrder.getId().toString())
+        ).build();
     }
 
 }

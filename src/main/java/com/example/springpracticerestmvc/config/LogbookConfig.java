74c0574654072a19d660e477e793cf1286af080f
noobdevsam <mdshoykotislam05@gmail.com>
2025-07-14 23:34:13 +0600

    docs: add JavaDoc comments to LogbookConfig class for improved code documentation

--- src/main/java/com/example/springpracticerestmvc/config/LogbookConfig.java
+++ src/main/java/com/example/springpracticerestmvc/config/LogbookConfig.java
@@ -7,13 +7,24 @@ import org.zalando.logbook.Sink;
 import org.zalando.logbook.json.JsonHttpLogFormatter;
 import org.zalando.logbook.logstash.LogstashLogbackSink;
 
+/**
+ * Configuration class for Logbook, a library for HTTP request and response logging.
+ */
 @Configuration
 public class LogbookConfig {
 
+    /**
+     * Creates a Logbook Sink that uses Logstash for logging.
+     * The sink formats HTTP logs as JSON and sends them to Logstash.
+     *
+     * @return a configured LogstashLogbackSink instance.
+     */
     @Bean
     public Sink logbookLogStash() {
+        // Formatter for HTTP logs in JSON format
         HttpLogFormatter formatter = new JsonHttpLogFormatter();
 
+        // Sink that sends formatted logs to Logstash
         return new LogstashLogbackSink(formatter);
     }
 }
